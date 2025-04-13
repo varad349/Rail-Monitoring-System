0378632383e9821f98eb2d9fc2a7a6601bba60f5
varad349 <128622546+varad349@users.noreply.github.com>
2025-04-13 12:59:15 +0530

    Update temperature_card.dart
    Removed Comments

--- lib/pages/widgets/stationDetails/temperature_card.dart
+++ lib/pages/widgets/stationDetails/temperature_card.dart
@@ -1,38 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class TemperatureCard extends StatefulWidget {
-//   @override
-//   _TemperatureCardState createState() => _TemperatureCardState();
-// }
-
-// class _TemperatureCardState extends State<TemperatureCard> {
-//   int temperature = 24; // Initial temperature value
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Expanded(
-//       child: Card(
-//         color: Colors.grey[850], // Dark theme card
-//         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-//         elevation: 5,
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               Icon(Icons.thermostat_outlined, size: 40, color: Colors.red),
-//               SizedBox(height: 10),
-//               Text('Temperature', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
-//               SizedBox(height: 10),
-//               Text('$temperature°C', style: TextStyle(fontSize: 16, color: Colors.white)),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'websocket_service.dart'; // Adjust the import as needed
